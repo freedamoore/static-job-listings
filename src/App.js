f065e5a847cb46fc75ce86f1d13d5feef25178ee
Freeda Moore <freedamoore@outlook.com>
2020-07-22 13:22:38 +1000

    initial reading and displaying data in json file. Also added filtering on clicking on language or tool

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import data from './data/data.json';
+
 function App() {
+
+  const [filterList, setFilterList] = useState([]);
+
+  const addToFilter = (e) => setFilterList([...filterList, e.target.innerText]);
+
+  const containsFilterItem = (filterArray, array1, array2) => {
+    for (let i = 0; i < filterArray.length; i++)
+      if (array1.includes(filterArray[i]) || array2.includes(filterArray[i])) {
+        return true;
+      }
+  }
+
+  let filteredjobs;
+  if (filterList.length) {
+    filteredjobs = data.filter(job => containsFilterItem(filterList, job.languages, job.tools));
+  } else {
+    filteredjobs = data;
+  }
+
+  console.log(filterList);
+  console.log(filteredjobs);
+
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+    < div className="App" >
+      {
+        filteredjobs.map(item => (
+          <div key={item.id}>
+            <h3> {item.company} </h3>
+            <ol>{item.languages.map((lang, i) => (<li key={i} onClick={addToFilter} style={{ color: 'blue' }}>{lang}</li>))}</ol>
+            <ul>{item.tools.map((tool, i) => (<li key={i} onClick={addToFilter}>{tool}</li>))}</ul>
+          </div>
+        ))
+      }
+    </div >
   );
 }
 
