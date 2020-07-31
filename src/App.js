33c2ebc5a3f26eeece0a132a2b58850ab13bfe83
Freeda Moore <freedamoore@outlook.com>
2020-07-31 11:08:10 +1000

    added responsive footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import './App.css';
 import Header from './components/Header';
-
+import Footer from './components/Footer';
+import './App.css';
 import data from './data/data.json';
 
 function App() {
@@ -39,6 +39,9 @@ function App() {
           </div>
         ))
       }
+
+      <Footer />
+
     </div >
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>
+                Challenge by <a href="https://www.frontendmentor.io/challenges" target="_blank">Frontend Mentor</a>.
+                Coded by <a href="https://freedamoore.github.io/">Freeda Moore</a>.
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
