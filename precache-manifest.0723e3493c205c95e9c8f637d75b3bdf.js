691c3675b025adcdc98e97f4191bb23d9dacf002
Freeda Moore <freedamoore@outlook.com>
2020-08-26 15:50:34 +1000

    Updates

---
+++ precache-manifest.0723e3493c205c95e9c8f637d75b3bdf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8309dac38cdd90e4f6db1b4e41e0b8c2",
+    "url": "/static-job-listings/index.html"
+  },
+  {
+    "revision": "41619c5d714c9aebc10b",
+    "url": "/static-job-listings/static/css/main.87853c39.chunk.css"
+  },
+  {
+    "revision": "2afb56738c5866d28f90",
+    "url": "/static-job-listings/static/js/2.672772a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static-job-listings/static/js/2.672772a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41619c5d714c9aebc10b",
+    "url": "/static-job-listings/static/js/main.be172ad9.chunk.js"
+  },
+  {
+    "revision": "fee4e05dc36b6f179ffe",
+    "url": "/static-job-listings/static/js/runtime-main.94596790.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/static-job-listings/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/static-job-listings/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/static-job-listings/static/media/icon-remove.ae67c678.svg"
+  }
+]);
